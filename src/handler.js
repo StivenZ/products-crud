b1c82c1ff153d2cd16d892dbfecb904b4305addf
stivenz <1806@holbertonschool.com>
2021-08-25 14:08:58 -0500

    Added lambda fnctions and CRUD operations

--- src/handler.js
+++ src/handler.js
@@ -1,10 +1,95 @@
+const connectToDatabase = require('../db/db.js')
+const ProductModel = require('../db/models/Product.Model.js')
+const dotenv = require( 'dotenv/config');
+
 'use strict';
 
-module.exports.generateRandomNumber = async (event) => {
-  const randomNumber = parseInt(Math.random() * 100);
-  console.log(`The random number is: ${randomNumber}`);
-  return randomNumber;
+module.exports.create = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+
+    connectToDatabase()
+      .then(() => {
+        ProductModel.create(JSON.parse(event.body))
+          .then(newProduct => callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(newProduct)
+          }))
+          .catch(err => callback(null, {
+            statusCode: err.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not create the note.'
+          }));
+      });
 };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.listOne = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      ProductModel.findById(event.pathParameters.id)
+        .then(ProductModel => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(ProductModel)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the ProductModel.'
+        }));
+    });
+};
+
+module.exports.listAll = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      ProductModel.find()
+        .then(products => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(products)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the ProductModels.'
+        }))
+    });
+};
+
+module.exports.edit = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      ProductModel.findByIdAndUpdate(event.pathParameters.id, JSON.parse(event.body), { new: true })
+        .then(product => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(product)
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the ProductModels.'
+        }));
+    });
+};
+
+module.exports.delete = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  connectToDatabase()
+    .then(() => {
+      ProductModel.findByIdAndRemove(event.pathParameters.id)
+        .then(product => callback(null, {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Removed ProductModel with id: ' + product._id, product: product })
+        }))
+        .catch(err => callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the ProductModels.'
+        }));
+    });
+};
